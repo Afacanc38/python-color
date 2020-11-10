a4ab9c5980048803e581762da808d91f985351b7
Alperen İsa <66299502+Afacanc38@users.noreply.github.com>
2020-11-10 12:04:51 +0300

    Program yüklendi

---
+++ PythonColor.py
@@ -0,0 +1,52 @@
+sıfır = "\033[0m"  # Öntanımlı hale getir
+
+egik = "\033[2m" # Eğik yazı
+aCizili = "\033[4m" # Altı Çizili
+tRenk = "\033[7m" # Ters renkler
+gYazi = "\033[8m" # Gizli yazı
+uCizili = "\033[9m" # Üstü çizili
+
+# Renkli yazı
+
+siyah = "\033[;30m" # Siyah
+kirmizi = "\033[;31m" # Kırmızı
+yesil = "\033[;32m" # Yeşil
+sari = "\033[;33m" # Sarı
+mavi = "\033[;34m" # Mavi
+pembe = "\033[;35m" # Pembe
+turkuaz = "\033[;36m" # Turkuaz
+beyaz = "\033[;37m" # Beyaz
+
+# Açık renkli yazı
+
+gri = "\033[;90m" # Gri yazı
+aKirmizi = "\033[;91m" # Açık kırmızı 
+aYesil = "\033[;92m" # Açık yeşil
+aSari = "\033[;93m" # Açık sarı
+aMavi = "\033[;94m" # Açık mavi
+aPembe = "\033[;95m" # Açık pembe
+aTurkuaz = "\033[;96m" # Açık turkuaz
+aBeyaz = "\033[;97m" # Açık beyaz
+
+# Artalan renkleri
+
+siyahA = "\033[;40m" # Siyah
+kirmiziA = "\033[;41m" # Kırmızı
+yesilA = "\033[;42m" # Yeşil
+sariA = "\033[;43m" # Sarı
+maviA = "\033[;44m" # Mavi
+pembeA = "\033[;45m" # Pembe
+turkuazA = "\033[;46m" # Turkuaz
+beyazA = "\033[;47m" # Beyaz
+
+# Açık artalan renkleri
+
+griA = "\033[;100m" # Gri
+aKirmiziA = "\033[;101m" # Açık kırmızı
+aYesilA = "\033[;102m" # Açık yeşil
+aSariA = "\033[;103m" # Açık sarı
+aMaviA = "\033[;104m" # Açık mavi
+aPembeA = "\033[;105m" # Açık pembe
+aTurkuazA = "\033[;106m" # Açık turkuaz
+aBeyazA = "\033[;107m" # Açık beyaz
+
